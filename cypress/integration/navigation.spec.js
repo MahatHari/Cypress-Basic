6f76eb9bfe80bf7e50994e3a3bc3ebf9cfdfb8c8
Hari Mahat <hmahat@gmail.com>
2021-09-03 11:15:41 +0300

    finnish comments

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,7 +5,7 @@ beforeEach(() => {
 });
 
 describe('Visit Cypress ', () => {
-  it.only('should navigate to get command and  assert border', () => {
+  it('should navigate to get command and  assert border', () => {
     cy.get('nav').within(() => {
       cy.contains('API').click();
     });
@@ -15,18 +15,28 @@ describe('Visit Cypress ', () => {
     });
     cy.get('#Get-vs-Find')
       .scrollIntoView()
-      //.should('be.visible')
       .should('have.css', 'border-bottom-width', '1px');
   });
 });
 describe('Test search functionality', () => {
-  it.only(' should take input and search for that input in modal and close modal', () => {
-    //
+  it(' should take input and search for that input in modal and close modal upon clicking any result list', () => {
+    //etsi elementti luokan nimellä '.DoSearch', klikka sitä, varmistä että se on epätarkka
     cy.get('.DocSearch', { timeout: 10000 }).click().should('not.be.focused');
+
+    //1. etsi elemennti tunnuksella '#docsearch-input"
+    //2. pitäisi olla näkyvissä
+    //3. kirjoita 'find'  syöttöruutun
+    //4. syöttöruutun pitäisi olla tarkkalla
     cy.get('#docsearch-input')
       .should('be.visible')
       .type('find')
       .should('be.focused');
+    //1. etsi elemennti luokan nimellä '.DocSearch-Dropdown"
+    //2. sen sisältä etsi elemennti tunnuksellä  '#docsearch-item-0"
+    //3. varmista että  se on sama tai vastava   syöttöruutun texti
+    //4. klikka sitä
+    //5. varmista modal on irotettu DOM:lta
+    //6. varmista url sisältää lyötyi'/api/commands/find'
     cy.get('.DocSearch-Dropdown')
       .find('#docsearch-item-0')
       .should('have.text', 'find')
